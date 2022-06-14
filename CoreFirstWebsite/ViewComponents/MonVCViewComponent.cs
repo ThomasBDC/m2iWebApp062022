bcb3a1f11f083dd8379c09a201ad59c76bbf6b84
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-06-14 16:42:55 +0200

    Ajout des viewComponents

---
+++ CoreFirstWebsite/ViewComponents/MonVCViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using CoreFirstWebsite.Models.Links;
+using m2iWebApp.Repository.Links;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreFirstWebsite
+{
+    public class MonVCViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //Mettre du code C#
+
+            var allLinks = LinksRepository.getAllLinks();
+
+            var vm = new LinksListViewModel
+            {
+                AllLinks = allLinks
+            };
+
+            //Ici on peut passer un modèle à la vue
+            return View("ViewOfMyVC", vm);
+        }
+    }
+}
